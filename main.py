5e410c187a3abc2b939f046913e121c47c61df2a
Vilmo18 <yvan.vilmorin@facsciences-uy1.cm>
2024-01-30 00:04:18 +0200

    change

--- main.py
+++ main.py
@@ -3,22 +3,24 @@ import cv2
 image = cv2.imread('hill.jpg')
 
 imagename='detection'
+image_modif='modify/'+imagename+'modif.jpg'
+
 rectangle_coordinates = (100, 50, 200, 150)
 
-rectangle_coordinates1 = (150, 100, 200, 150)
 
 x, y, w, h = rectangle_coordinates
-x1, y1, w1,h1 = rectangle_coordinates1
 
 cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-cv2.rectangle(image, (x1, y1), (x1 + w1, y1 + h1), (0, 255, 0), 2)
 
 
+detection = "Votre texte ici"
+police=cv2.FONT_HERSHEY_SIMPLEX
+scale=0.5
+cv2.putText(image, detection, (x, y - 10), police, scale, (255, 0, 0), )
 
-text = "Votre texte ici"
-cv2.putText(image, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
-cv2.putText(image, text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-cv2.imwrite('hills.jpg',image)
+cv2.imwrite(image_modif,image)
+
 cv2.imshow(imagename, image)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
